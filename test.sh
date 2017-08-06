eb0de6f32912e1e49db1fea87c1d419ab508a1b3
pib219 <pib219@gmail.com>
2017-08-07 01:45:41 +0800

    test sh for minecraft server

---
+++ test.sh
@@ -0,0 +1,1 @@
+java -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=4M -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar spigot.jar nogui
